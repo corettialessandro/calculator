cf6c14f9853a16b3d4e460d4f21fee422581747c
Alessandro Coretti <alessandro.coretti@polito.it>
2022-04-23 16:40:04 +0200

    Modified operation printout function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from std_operations import Add
+from std_operations import Add, Subtract
 
 a = -1
 b = 3.14
 
 c = Add(a, b)
+d = Subtract(a, b)
 
-print("{} + {} = {}".format(a, b, c))
+print("{} + {} = {}\n{} - {} = {}".format(a, b, c, a, b, d))
